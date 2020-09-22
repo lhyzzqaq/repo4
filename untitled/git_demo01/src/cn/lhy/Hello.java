eb33ff9edc51b975edb72526723d22058f1203b6
lhy <1277677972@qq.com>
2020-09-22 16:15:10 +0800

    第一次提交

--- untitled/git_demo01/src/cn/lhy/Hello.java
+++ untitled/git_demo01/src/cn/lhy/Hello.java
@@ -2,5 +2,10 @@ package cn.lhy;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("sad");
+
+    }
+
+    public void test(){
+        System.out.println("test");
     }
 }
